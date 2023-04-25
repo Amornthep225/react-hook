d2ed69787b10ecaaeab2a0e0c3a7b5ba5016aa09
Amornthep22509 <651463030@crru.ac.th>
2023-04-25 11:23:16 +0700

    first commit

--- src/pages/UseEffect/index.jsx
+++ src/pages/UseEffect/index.jsx
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import { useState, useEffect } from 'react'
 
-export class UseEffect extends Component {
-  render() {
-    return (
-      <div>UseEffect</div>
-    )
-  }
+const UseEffect = () => {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('CRRU DEV')
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    if (count > 0 && count <= 10) {
+      fetch(`https://jsonplaceholder.typicode.com/users/${count}`).then((res) =>
+        res.json().then((data) => setUser(data))
+      )
+    }
+  }, [count])
+
+  return (
+    <div className='App'>
+      <h1>Hello {name}</h1>
+      <button type='button' onClick={() => setName(user.name || 'Missing')}>
+        Update Name
+      </button>
+      <h3>{user.name || 'Unknow'}</h3>
+      <div className='card'>
+        <h2>{count}</h2>
+        <button type='button' onClick={() => setCount(count - 1)}>
+          Decrease
+        </button>
+        <button type='button' onClick={() => setCount(count + 1)}>
+          Increase
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default UseEffect
